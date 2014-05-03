305afd5623dcefc400b3cffa129299fc0476b4bf
John Lane <john@lane.uk.net>
2014-05-03 12:18:32 +0100

    pagination working

--- app/widgets/items_widget.rb
+++ app/widgets/items_widget.rb
@@ -1,10 +1,33 @@
 class ItemsWidget < Apotomo::Widget
 
   def display
-    Item.all.paginate(page: params[:page], per_page: 10).each do |i|
+    items = Item.all.paginate(page: params[:page], per_page: 10)
+    items.each do |i|
       self << widget('items/item', i, item: i)
     end
-    render
+    render locals: { page: params[:page], pages: items.total_pages }
   end
+
+  private
+  def paginate(args)
+    page = args[:page] ? args[:page].to_i : 1
+    pages = args[:pages] ? args[:pages].to_i : 1
+
+    first_css = %{class="disabled"} if page == 1
+    last_css  = %{class="disabled"} if page == pages
+
+    links = %{<ul class="pagination">\n}
+    links << %{  <li #{first_css}><a href="#{url_for}">&laquo;</a></li>\n}
+    1.upto pages do |p|
+      current_css = %{class="active"} if p == page
+      links << %{  <li #{current_css}><a href="#{url_for(params.merge({page: p}))}">#{p}</a></li>\n}
+    end
+    links << %{  <li #{last_css}><a href="#{url_for}?page=#{pages}">&raquo;</a></li>\n}
+    links << %{</ul>\n}
+
+    links.html_safe
+
+  end
+  helper_method :paginate
 
 end

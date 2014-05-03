1c892512650dafb4c9832f04bbc97041a2274f4e
John Lane <john@lane.uk.net>
2014-05-03 10:49:34 +0100

    pagination part-working

--- app/widgets/items_widget.rb
+++ app/widgets/items_widget.rb
@@ -1,7 +1,7 @@
 class ItemsWidget < Apotomo::Widget
 
   def display
-    Item.all.paginate.each do |i|
+    Item.all.paginate(page: params[:page], per_page: 10).each do |i|
       self << widget('items/item', i, item: i)
     end
     render
